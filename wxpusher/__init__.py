7aade307cf3d2601f7c56e9eb7ed9175610de630
huxuan <i@huxuan.org>
2020-01-17 15:59:51 +0800

    Version 0.1.0, repo migration with better VERSION.

---
+++ wxpusher/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: __init__.py
+Author: huxuan
+Email: i(at)huxuan.org
+Description: init for WxPusher.
+"""
+from .wxpusher import WxPusher
+from .exceptions import WxPusherException
+from .exceptions import WxPusherNoneTokenException
+
+__all__ = [
+    'WxPusher',
+    'WxPusherException',
+    'WxPusherNoneTokenException'
+]
+
+__version__ = open('VERSION').read().strip()
