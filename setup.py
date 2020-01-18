83a0c0b92dd09dbf527be2ff37aa527b4469d4c6
huxuan <i@huxuan.org>
2020-01-18 14:41:23 +0800

    Fix README url error when uploading to PyPI.

--- setup.py
+++ setup.py
@@ -41,15 +41,20 @@ DESCRIPTION = (
     'WxPusher Python SDK.'
 )
 
+VERSION = open('VERSION').read().strip()
+PROJECT_URL = 'https://github.com/wxpusher/wxpusher-sdk-python'
+BASE_URL= f'{PROJECT_URL}/blob/v{VERSION}'
 
 def readme():
     """Parse README for long_description."""
-    with open('README.md') as fin:
-        return fin.read()
+    content = open('README.md').read()
+    content = content.replace('README-en.md', f'{BASE_URL}/README-en.md', 1)
+    content = content.replace('README.md', f'{BASE_URL}/README.md', 1)
+    return content
 
 
 setup(name='wxpusher',
-      version=open('VERSION').read().strip(),
+      version=VERSION,
       description=DESCRIPTION,
       long_description=readme(),
       long_description_content_type='text/markdown',
