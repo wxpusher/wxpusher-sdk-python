54a9091cb89cd0fd4d955a9de047f764e0f2a14a
huxuan <i@huxuan.org>
2020-02-16 21:41:53 +0800

    Rename get_token to _get_token.

--- wxpusher/wxpusher.py
+++ wxpusher/wxpusher.py
@@ -21,7 +21,7 @@ class WxPusher():
     def send_message(cls, content, **kwargs):
         """Send Message."""
         payload = {
-            'appToken': cls.get_token(kwargs.get('token')),
+            'appToken': cls._get_token(kwargs.get('token')),
             'content': content,
             'contentType': kwargs.get('content_type', 1),
             'topicIds': kwargs.get('topic_ids', []),
@@ -41,7 +41,7 @@ class WxPusher():
     def create_qrcode(cls, extra, valid_time=1800, token=None):
         """Create qrcode with extra callback information."""
         payload = {
-            'appToken': cls.get_token(token),
+            'appToken': cls._get_token(token),
             'extra': extra,
             'validTime': valid_time
         }
@@ -52,7 +52,7 @@ class WxPusher():
     def query_user(cls, page, page_size, token=None):
         """Query users."""
         payload = {
-            'appToken': cls.get_token(token),
+            'appToken': cls._get_token(token),
             'page': page,
             'pageSize': page_size
         }
@@ -60,7 +60,7 @@ class WxPusher():
         return requests.get(url, params=payload).json()
 
     @classmethod
-    def get_token(cls, token=None):
+    def _get_token(cls, token=None):
         """Get token with validation."""
         token = token or cls.default_token
         if not token:
